3fa58ce902fae4aa11ae7d5a824f90bb056844b1
Abdelhakim Nassif <anassif@e2r10p5.1337.ma>
2023-01-22 20:59:28 +0100

    LOL

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.goat.avaj;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
---
+++ aircraft/Coordinates.java
@@ -0,0 +1,45 @@
+package com.goat.avaj.aircraft;
+
+public class Coordinates {
+    private int longitude;
+    private int latitude;
+    private int height;
+
+    public Coordinates(int longitude, int latitude, int height) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.height = height;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", height=" + height +
+                '}';
+    }
+
+//    @Override
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//
+//        return super.equals(obj);
+//    }
+}
