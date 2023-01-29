1c4d8a27cb6a4ed2dcba2af69aa6c5168d65b6cf
anassif <hakimoxxx@gmail.com>
2023-01-30 00:12:24 +0100

    update conditions

--- com/goat/avaj/Main.java
+++ com/goat/avaj/Main.java
@@ -1,6 +1,7 @@
 package com.goat.avaj;
 
 
+import com.goat.avaj.WeatherProvider.WeatherTower;
 import com.goat.avaj.aircraft.AircraftFactory;
 import com.goat.avaj.flayable.Flayable;
 
@@ -17,25 +18,24 @@ public class Main {
             inFile = new File(args[0]);
         }
         else {
-            System.err.println("invalide arguments count: " + args.length);
+            System.err.println("Invalid arguments count: " + args.length);
             System.exit(1);
         }
         BufferedReader br = null;
         try {
             String sCurrentLine;
             br = new BufferedReader(new FileReader(inFile));
-            int Nupdates = Integer.parseInt(br.readLine());
-            if(Nupdates < 0) throw new java.io.IOException("bad argument !");
+            int NUpdates = Integer.parseInt(br.readLine());
+            if(NUpdates < 0) throw new java.io.IOException("bad argument !");
             while ((sCurrentLine = br.readLine()) != null) {
 //                System.out.println(sCurrentLine);
                 
                 String[] splitted = sCurrentLine.split(" ");
-//                for(String s : splitted )
-//                    System.out.println(s);
               if(splitted.length == 5){
-                Flayable f = new AircraftFactory().newAircraft(splitted[0], splitted[1] , Integer.parseInt(splitted[2]), Integer.parseInt(splitted[3]), Integer.parseInt(splitted[4]));
-//
+                Flayable f = new AircraftFactory().newAircraft(splitted[0], splitted[1] , Integer.parseInt(splitted[2]),
+                        Integer.parseInt(splitted[3]), Integer.parseInt(splitted[4]));
 //                System.out.println(f.toString());
+                  f.registerTower(new WeatherTower());
                 }
                 else throw new IOException();
             }
@@ -52,22 +52,5 @@ public class Main {
                 System.exit(1);
             }
         }
-//        File inFile = null;
-//
-//        StringBuilder sb = new StringBuilder();
-//        for (String s: args)
-//            sb.append(String.format("%s\n", s));
-//        System.out.println(sb);
-
-//        sb.append(123);
-//        sb.append(
-//                12.123
-//        );
-//        sb.append("asdf", 0, 3);
-//
-//
-//        System.out.println(sb);
-
-    }
-
+}
 }
