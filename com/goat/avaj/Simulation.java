dbf3fbe99ff39034d76c27337406a49c03904a06
anassif <hakimoxxx@gmail.com>
2023-02-16 22:31:27 +0100

    badarguments

--- com/goat/avaj/Simulation.java
+++ com/goat/avaj/Simulation.java
@@ -30,7 +30,7 @@ public class Simulation {
             String sCurrentLine;
             br = new BufferedReader(new FileReader(inFile));
             NUpdates = Integer.parseInt(br.readLine());
-            if(NUpdates <= 0) throw new IOException("number needs to be positive ! ==>" + NUpdates);
+            if(NUpdates <= 0) throw new BadArgument("number needs to be positive ! ==>" + NUpdates);
             while ((sCurrentLine = br.readLine()) != null) {
 //                System.out.println(sCurrentLine);
                 String[] splitted = sCurrentLine.split(" ");
@@ -39,15 +39,15 @@ public class Simulation {
                     Flayable f = new AircraftFactory().newAircraft(splitted[0], splitted[1] , Integer.parseInt(splitted[2]),
                             Integer.parseInt(splitted[3]), (Math.min(Integer.parseInt(splitted[4]), 100)));
 //                System.out.println(f.toString());
-                    f.registerTower(weatherTower);
+                    f.registerTower(weatherTower); // register the aircraft to the weather tower
                 }
                 else throw new BadArgument("must be a set of 5 arguments with positive coordinates !");
             }
             WeatherProvider weatherProvider = WeatherProvider.getProvider();
-            for(int i = 0 ; i < NUpdates; i++)
+            for(int i = 0 ; i < NUpdates; i++) //change the weather N times(NUpdates)
                 weatherTower.changeWeather();
         }
-        catch (IOException e) {
+        catch (BadArgument |  IOException e) {
             throw new BadArgument("Invalid input " + e);
 
         }
